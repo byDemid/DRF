c4b677d05474c214cd346f348273932d126b4145
Demid <bydimko@yandex.ru>
2021-11-11 01:04:46 +0300

    lesson4 updated views

--- TODO/users/views.py
+++ TODO/users/views.py
@@ -1,8 +1,17 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import TodoUser
 from .serializers import UserModelSerializer
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework import mixins
 
 
-class UserModelViewSet(ModelViewSet):
+# class UserModelViewSet(ModelViewSet):
+#     queryset = TodoUser.objects.all()
+#     serializer_class = UserModelSerializer
+
+
+class UserModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                               mixins.UpdateModelMixin, GenericViewSet):
     queryset = TodoUser.objects.all()
     serializer_class = UserModelSerializer
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
