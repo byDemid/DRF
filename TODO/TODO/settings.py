d411ada74c68da76e3f8965b37e25156f59a8f48
Demid <bydimko@yandex.ru>
2021-11-11 01:02:53 +0300

    lesson4 REST_FRAMEWORK

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'users',
     'corsheaders',
     'project',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.TodoUser"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
